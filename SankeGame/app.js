ac0c5591c4624122af354855d33bd30f1871e2c1
omyadav786 <ayurashiyadav143@gmail.com>
2026-08-12 13:15:06 +0000

    update snake game

--- SankeGame/app.js
+++ SankeGame/app.js
@@ -1,1 +1,10 @@
-console.log("hello world");
+const board = document.querySelector('.board');
+const boardHeight = 30;
+const boardWidth = 30;
+const row = Math.floor(board.clientHeight/boardHeight);
+const col = Math.floor(board.clientWidth/boardWidth);
+for(let i=0; i<row*col; i++){
+    const block = document.createElement('div');
+    block.classList.add("block");
+    board.appendChild(block);
+}
